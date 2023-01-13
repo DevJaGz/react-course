29cfaf4f8c7128a01f52cbe9bbf2b663090f8037
Julian Gomez <jgomez109@unab.edu.co>
2023-01-13 00:06:13 -0500

    Create Vite project

---
+++ hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export function App() {
+    return(
+        <article>
+            <header>
+                <img 
+                src="https://static-cdn.jtvnw.net/jtv_user_pictures/47074035-e636-47d4-b935-61c74409e299-profile_image-70x70.png" alt="Avatar" />
+                <div>
+                    <strong>Kelly Alejandra castañeda</strong>
+                    <span>@Account</span>
+                </div>
+            </header>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
